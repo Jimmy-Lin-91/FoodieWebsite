ac184bb388e7ae84b0879ba3ff9d2ec478e2907d
Jimmy Lin <jimmy.lin0402@gmail.com>
2021-04-08 15:55:31 -0700

    Navbar and order buttons complete.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,51 @@
 import React from 'react';
-
-class App extends React.Component {
-  constructor(props){
-    super(props),
-    this.state = {}
-  }
-  render(){
-    return (
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
+import GroupDining from './components/GroupDining.jsx';
+import GiftCards from './components/GiftCards.jsx';
+import Catering from './components/Catering.jsx';
+const App = () => {
+  return (
+    <Router>
       <div>
-        <h1> Hello!</h1>
+        <nav class='nav-container'>
+          <ul class='nav'>
+            <li>
+              <Link to='/' class='link name'>The Squeeze</Link>
+            </li>
+            <li>
+              <Link to='/group' class='link'>Group Dining</Link>
+            </li>
+            <li>
+              <Link to='/giftcards' class='link'>Gift Cards</Link>
+            </li>
+            <li>
+              <Link to='/catering' class='link'>Catering</Link>
+            </li>
+          </ul>
+          <div class='button__container'>
+            <a class='order_btn'>Order Delivery</a>
+            <a class='order_btn'>Book a Table</a>
+          </div>
+        </nav>
+        <Switch>
+          <Route path='/group'>
+            <GroupDining/>
+          </Route>
+          <Route path='/giftcards'>
+            <GiftCards/>
+          </Route>
+          <Route path='/catering'>
+            <Catering/>
+          </Route>
+        </Switch>
       </div>
-    )
-  }
+    </Router>
+  )
+
 }
 export default App;
